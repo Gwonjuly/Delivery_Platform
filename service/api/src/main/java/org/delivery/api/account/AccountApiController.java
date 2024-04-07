eb9300ecedd12d16c984c21a51ad9184bfd3345a
July <103982139+Gwonjuly@users.noreply.github.com>
2024-04-07 22:13:23 +0900

    JsonNaming -> ObjectMapper로 빈 등록

--- service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,14 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -13,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountApiController {
 
     private final AccountRepository accountRepository;//JpaConfig: db(다른 패키지)에 있는 것을 빈으로 등록하기 위해서 Configuration 필요
-    @GetMapping("")
-    public void save(){
-        var account= AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("wnfl@naver.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
