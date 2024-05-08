24211a39aab6049bc3b2fb01a5ddc84b1c4f9c7a
July <103982139+Gwonjuly@users.noreply.github.com>
2024-05-08 22:28:20 +0900

    User Order 서비스 로직 상세 구현: - 주문 생성, 주문 상태 변경 - 주문 확인/조리/픽업 등등 - 주문 내역 확인: 현재 진행 중 및 과거 주문 내역

--- service/api/src/main/java/org/delivery/api/domain/userorder/service/UserOrderService.java
+++ service/api/src/main/java/org/delivery/api/domain/userorder/service/UserOrderService.java
@@ -24,25 +24,74 @@ public class UserOrderService {
 
     //특정 유저의 특정 주문 가져오기
     public UserOrderEntity getUserOrderWithThrow(Long id, Long userId){
-        return userOrderRepository.findAllFirstByIdAndStatusAndUserID(id, UserOrderStatus.REGISTERED,userId)
+        return userOrderRepository.findAllFirstByIdAndStatusAndUserId(id, UserOrderStatus.REGISTERED,userId)
                 .orElseThrow(()->new ApiException(ErrorCode.NULL_POINT));
     }
 
     //특정 유저의 모든 주문 가져오기
     public List<UserOrderEntity> getUserOrderList(Long userId){
-        return userOrderRepository.findAllByUserIDAndStatusOrderByIdDesc(userId,UserOrderStatus.REGISTERED);
+        return userOrderRepository.findAllByUserIdAndStatusOrderByIdDesc(userId,UserOrderStatus.REGISTERED);
     }
 
     public List<UserOrderEntity> getUserOrderList(Long userId, List<UserOrderStatus> statusList){
         return userOrderRepository.findAllByUserIdAndStatusInOrderByIdDesc(userId,statusList);
     }
-    //주문 생성
+    //주문 생성(create)
+    public UserOrderEntity order(UserOrderEntity userOrderEntity){
+        return Optional.ofNullable(userOrderEntity)
+                .map(it->{
+                    userOrderEntity.setStatus(UserOrderStatus.ORDER);
+                    it.setOrderedAt(LocalDateTime.now());
+                    return userOrderRepository.save(it);
+                })
+                .orElseThrow(()->new ApiException(ErrorCode.NULL_POINT));
+    }
+    //지금 현재 진행 중인 나의 주문 내역
+    public List<UserOrderEntity> current(Long userId){
+        return getUserOrderList(userId,
+                List.of(
+                        UserOrderStatus.ORDER,
+                        UserOrderStatus.COOKING,
+                        UserOrderStatus.DELIVERY,
+                        UserOrderStatus.ACCEPT
+                ));
+    }
 
-    //가게 주문 확인
+    //과거에 주문했던 내역
+    public List<UserOrderEntity> history(Long userId){
+        return getUserOrderList(userId,
+                List.of(
+                        UserOrderStatus.RECEIVE
+                ));
+    }
 
-    //가게 조리 시자작
+    //주문 상태 변경 method:status
+    public UserOrderEntity setStatus(UserOrderEntity userOrderEntity, UserOrderStatus userOrderStatus){
+        userOrderEntity.setStatus(userOrderStatus);//entity 검색 후, save: insert(신규 추가)가 아닌 update
+        return userOrderRepository.save(userOrderEntity);
+    }
+
+    //가게 주문 확인: accepted_at
+    public UserOrderEntity accept(UserOrderEntity userOrderEntity){
+         userOrderEntity.setAcceptedAt(LocalDateTime.now());
+         return setStatus(userOrderEntity,UserOrderStatus.ACCEPT);
+    }
+
+    //가게 조리 시작
+    public UserOrderEntity cooking(UserOrderEntity userOrderEntity){
+        userOrderEntity.setCookingStartedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.COOKING);
+    }
 
     //라이더 배달 시작
+    public UserOrderEntity delivery(UserOrderEntity userOrderEntity){
+        userOrderEntity.setDeliveryStartedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.DELIVERY);
+    }
 
     //배달 완료
+    public UserOrderEntity receive(UserOrderEntity userOrderEntity){
+        userOrderEntity.setReceivedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.RECEIVE);
+    }
 }
--- service/db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
+++ service/db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
@@ -11,11 +11,11 @@ public interface UserOrderRepository extends JpaRepository<UserOrderEntity, Long
     //특정 유저의 모든 주문 내역 가져오기
     //특정 유저: user_order의 user_id
     //select * from user_order where user_id = ? and status =? order by id desc limit 1
-    List<UserOrderEntity> findAllByUserIDAndStatusOrderByIdDesc(Long userId, UserOrderStatus status);
+    List<UserOrderEntity> findAllByUserIdAndStatusOrderByIdDesc(Long userId, UserOrderStatus status);
 
     //특정 유저의 특정 주문 내역 가져오기
     //특정 유저: user_order의 user_id, 특정 주문: user_order의 id
-    Optional<UserOrderEntity> findAllFirstByIdAndStatusAndUserID(Long id, UserOrderStatus status, Long userId);
+    Optional<UserOrderEntity> findAllFirstByIdAndStatusAndUserId(Long id, UserOrderStatus status, Long userId);
 
     //select * from user_order where user_id=? and status in (?,?..) order by id desc, in: 복수 개로 select
     List<UserOrderEntity> findAllByUserIdAndStatusInOrderByIdDesc(Long userId, List<UserOrderStatus> status);
