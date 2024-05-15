7da1e6cfdad4ae313d305de55f5c309bbe9473d3
July <103982139+Gwonjuly@users.noreply.github.com>
2024-05-15 22:08:43 +0900

    [가맹점 서버 개발: 가맹점 사용자 정보 확장] UserDetails 확장: UserDetails(로그인 시, 저장되는 사용자 정보(UserSession)

--- service/store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
+++ service/store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
@@ -2,7 +2,10 @@ package org.delivery.storeadmin.domain.authorization;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.http.parser.Authorization;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreStatus;
 import org.delivery.db.storeuser.enums.StoreUserStatus;
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
 import org.delivery.storeadmin.domain.user.service.StoreUserService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,20 +18,31 @@ import org.springframework.stereotype.Service;
 public class AuthorizationService implements UserDetailsService {
 
     private final StoreUserService storeUserService;
+    private final StoreRepository storeRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         // 웹에서 입력한 username(email)로 사용자가 있는지크
         var storeUserEntity=storeUserService.getRegisterUser(username);
+        var storeEntity=storeRepository.findFirstByIdAndStatusOrderByIdDesc(storeUserEntity.get().getStoreId(), StoreStatus.REGISTERED);
 
         return storeUserEntity.map(it->{// 사용자가 있으면 storeUserEntity의 email과 pw를 User(security 제공)에 담아 리턴
-            var user= User.builder()
-                    .username(it.getEmail())
+            //로그인 시, 저장되는 사용자 정보 확장
+            var userSession= UserSession.builder()
+                    .userId(it.getId())
                     .password(it.getPassword())
-                    .roles(it.getRole().toString())
+                    .email(it.getEmail())
+                    .status(it.getStatus())
+                    .role(it.getRole())
+                    .registeredAt(it.getRegisteredAt())
+                    .lastLoginAt(it.getLastLoginAt())
+                    .unregisteredAt(it.getUnregisteredAt())
+                    .storeId(storeEntity.get().getId())
+                    .storeName(storeEntity.get().getName())
                     .build();
-            return user;
+
+            return userSession; //UserSession은 UserDetails 상속 받음
         }).orElseThrow(()->new UsernameNotFoundException(username));
         /*
         Spring-security:
--- service/store-admin/src/main/java/org/delivery/storeadmin/domain/user/converter/StoreUserConverter.java
+++ service/store-admin/src/main/java/org/delivery/storeadmin/domain/user/converter/StoreUserConverter.java
@@ -5,6 +5,7 @@ import org.delivery.db.store.StoreEntity;
 import org.delivery.db.store.StoreRepository;
 import org.delivery.db.store.enums.StoreStatus;
 import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.authorization.model.UserSession;
 import org.delivery.storeadmin.domain.user.controller.model.StoreUserRegisterRequest;
 import org.delivery.storeadmin.domain.user.controller.model.StoreUserResponse;
 import org.springframework.stereotype.Service;
@@ -29,7 +30,7 @@ public class StoreUserConverter {
         return StoreUserResponse.builder()
                 .user(
                         StoreUserResponse.UserResponse.builder()
-                                .id(storeEntity.getId())
+                                .id(storeUserEntity.getId())
                                 .email(storeUserEntity.getEmail())
                                 .status(storeUserEntity.getStatus())
                                 .role(storeUserEntity.getRole())
@@ -42,6 +43,28 @@ public class StoreUserConverter {
                         StoreUserResponse.StoreResponse.builder()
                                 .id(storeEntity.getId())
                                 .name(storeEntity.getName())
+                                .build()
+                )
+                .build();
+    }
+
+    public StoreUserResponse toResponse(UserSession userSession){
+        return StoreUserResponse.builder()
+                .user(
+                        StoreUserResponse.UserResponse.builder()
+                                .id(userSession.getUserId())
+                                .email(userSession.getEmail())
+                                .status(userSession.getStatus())
+                                .role(userSession.getRole())
+                                .registeredAt(userSession.getRegisteredAt())
+                                .unregisteredAt(userSession.getUnregisteredAt())
+                                .lastLoginAt(userSession.getLastLoginAt())
+                                .build()
+                )
+                .store(
+                        StoreUserResponse.StoreResponse.builder()
+                                .id(userSession.getStoreId())
+                                .name(userSession.getStoreName())
                                 .build()
                 )
                 .build();
