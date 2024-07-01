de46d8400437358bc8d8b05592d0813ca0a78d0c
Gwonjuly <wnfl0925@naver.com>
2024-07-01 23:50:41 +0900

    주문수락/조리시작 등 버튼을 누르면 주문 내역의 상태가 변경되도록 수정

--- service/store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/service/UserOrderService.java
+++ service/store-admin/src/main/java/org/delivery/storeadmin/domain/userorder/service/UserOrderService.java
@@ -3,8 +3,10 @@ package org.delivery.storeadmin.domain.userorder.service;
 import lombok.RequiredArgsConstructor;
 import org.delivery.db.userorder.UserOrderEntity;
 import org.delivery.db.userorder.UserOrderRepository;
+import org.delivery.db.userorder.enums.UserOrderStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -16,5 +18,35 @@ public class UserOrderService {
     //주문이 들어왔을 때, 해당 정보 반환
     public Optional<UserOrderEntity> getUserOrder(Long userOrderId){
         return  userOrderRepository.findById(userOrderId);
+    }
+
+    //주문 상태 변경 method:status
+    public UserOrderEntity setStatus(UserOrderEntity userOrderEntity, UserOrderStatus userOrderStatus){
+        userOrderEntity.setStatus(userOrderStatus);//entity 검색 후, save: insert(신규 추가)가 아닌 update
+        return userOrderRepository.save(userOrderEntity);
+    }
+
+    //가게 주문 확인: accepted_at
+    public UserOrderEntity accept(UserOrderEntity userOrderEntity){
+        userOrderEntity.setAcceptedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.ACCEPT);
+    }
+
+    //가게 조리 시작
+    public UserOrderEntity cooking(UserOrderEntity userOrderEntity){
+        userOrderEntity.setCookingStartedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.COOKING);
+    }
+
+    //라이더 배달 시작
+    public UserOrderEntity delivery(UserOrderEntity userOrderEntity){
+        userOrderEntity.setDeliveryStartedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.DELIVERY);
+    }
+
+    //배달 완료
+    public UserOrderEntity receive(UserOrderEntity userOrderEntity){
+        userOrderEntity.setReceivedAt(LocalDateTime.now());
+        return setStatus(userOrderEntity,UserOrderStatus.RECEIVE);
     }
 }
