ad9192a6443d0700823b977ba657f863aabf3512
Gwonjulee <wnfl0925@naver.com>
2024-08-06 23:05:09 +0900

    takeout 모듈 생성

---
+++ service/takeout/src/main/java/org/delivery/takeout/TakeOutApplication.java
@@ -0,0 +1,11 @@
+package org.delivery.takeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeOutApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TakeOutApplication.class, args);
+    }
+}
