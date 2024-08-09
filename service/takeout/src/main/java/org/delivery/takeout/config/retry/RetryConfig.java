363b5438c08ee9d0ee28f921a9c9be26177204b2
Gwonjulee <wnfl0925@naver.com>
2024-08-09 20:06:47 +0900

    spring-retry: retry 및 recover 메서드 작성

---
+++ service/takeout/src/main/java/org/delivery/takeout/config/retry/RetryConfig.java
@@ -0,0 +1,14 @@
+package org.delivery.takeout.config.retry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+    /*@Bean
+    public RetryTemplate retryTemplate(){
+        return new RetryTemplate();
+    }*/
+}
