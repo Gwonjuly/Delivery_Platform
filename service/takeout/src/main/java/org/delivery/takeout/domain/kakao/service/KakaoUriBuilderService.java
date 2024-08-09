cf30efd9e756b73e760cc4620a2b05afd54bc447
Gwonjulee <wnfl0925@naver.com>
2024-08-09 22:16:16 +0900

    KakaoSearchCategory: kakao 카테고리 기반(음식점) 서비스

--- service/takeout/src/main/java/org/delivery/takeout/domain/kakao/service/KakaoUriBuilderService.java
+++ service/takeout/src/main/java/org/delivery/takeout/domain/kakao/service/KakaoUriBuilderService.java
@@ -14,6 +14,8 @@ public class KakaoUriBuilderService {
 
     private static final String KAKAO_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
 
+    private static final String KAKAO_SEARCH_CATEGORY_URL = "https://dapi.kakao.com/v2/local/search/category.json";
+
     public URI buildUriByAddress (String address){
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
                 .fromHttpUrl(KAKAO_SEARCH_ADDRESS_URL)
@@ -21,6 +23,23 @@ public class KakaoUriBuilderService {
 
         URI uri = uriComponentsBuilder.build().encode().toUri();
         log.info("KakaoUriBuilderService-address: {}",address);
+        log.info("KakaoUriBuilderService-uri:{}",uri);
+        return uri;
+    }
+
+    public URI buildUrByCategory(double latitude, double longitude, double radius, String category){
+        double meterRadius = radius * 100;
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromHttpUrl(KAKAO_SEARCH_CATEGORY_URL)
+                .queryParam("category_group_code",category)
+                .queryParam("x",longitude)
+                .queryParam("y",latitude)
+                .queryParam("radius",meterRadius)
+                .queryParam("sort","distance");
+
+        URI uri = uriComponentsBuilder.build().encode().toUri();
+
         log.info("KakaoUriBuilderService-uri:{}",uri);
         return uri;
     }
