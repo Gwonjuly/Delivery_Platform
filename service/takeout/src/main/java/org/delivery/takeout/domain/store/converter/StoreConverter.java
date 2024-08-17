9b4eb1efb1a98bde9329b60a9dd19e11418bf4d0
Gwonjulee <wnfl0925@naver.com>
2024-08-17 21:36:23 +0900

    DirectionService: 길찾기 및 로드맵 url 추가

--- service/takeout/src/main/java/org/delivery/takeout/domain/store/converter/StoreConverter.java
+++ service/takeout/src/main/java/org/delivery/takeout/domain/store/converter/StoreConverter.java
@@ -1,13 +1,18 @@
 package org.delivery.takeout.domain.store.converter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.delivery.common.annotation.Converter;
 import org.delivery.db.direction.DirectionEntity;
 import org.delivery.db.store.StoreEntity;
 import org.delivery.takeout.domain.direction.controller.model.DirectionResponse;
 import org.delivery.takeout.domain.store.model.StoreDto;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Converter
+@Slf4j
 public class StoreConverter {
+    private static final String ROAD_VIEW_DEFAULT_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_MAP_DEFAULT_URL = "https://map.kakao.com/link/MAP/";
 
     public StoreDto toStoreDto(StoreEntity storeEntity){
         return StoreDto.builder()
@@ -20,12 +25,22 @@ public class StoreConverter {
     }
 
     public DirectionResponse toDirectionResponse(DirectionEntity directionEntity){
+
+        String params = String.join(",", directionEntity.getTargetStoreName(),
+                String.valueOf(directionEntity.getTargetLatitude()),
+                String.valueOf(directionEntity.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_MAP_DEFAULT_URL + params)
+                .toUriString();
+
+        log.info("direction params: {}, url:{}",params,result);
+
         return DirectionResponse.builder()
                 .addressName(directionEntity.getTargetAddress())
                 .storeName(directionEntity.getTargetStoreName())
                 .distance(String.format("%.2f km",directionEntity.getDistance()))
-                .directionUrl("direction Url")
-                .roadViewUrl("road view Url")
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_DEFAULT_URL + directionEntity.getTargetLatitude() + "," + directionEntity.getTargetLongitude())
                 .build();
     }
 }
