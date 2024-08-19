331e0a1783de5db637ba1ff0552de7b1ca44cf00
Gwonjulee <wnfl0925@naver.com>
2024-08-19 21:43:09 +0900

    takeout 모듈: redis 추가 및 test code 작성 - 스토어 조회 시 DB -> Redis를 사용하여 조회하도록 변경

--- service/takeout/src/main/java/org/delivery/takeout/domain/store/service/StoreSearchService.java
+++ service/takeout/src/main/java/org/delivery/takeout/domain/store/service/StoreSearchService.java
@@ -2,6 +2,7 @@ package org.delivery.takeout.domain.store.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.delivery.takeout.domain.store.cache.StoreRedisTemplateService;
 import org.delivery.takeout.domain.store.converter.StoreConverter;
 import org.delivery.takeout.domain.store.model.StoreDto;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,20 @@ public class StoreSearchService {
 
     private final StoreRepositoryService storeRepositoryService;
     private final StoreConverter storeConverter;
+    private final StoreRedisTemplateService storeRedisTemplateService;
 
     public List<StoreDto> searchStoreDtoList(){
+
+        //redis 조회
+        List<StoreDto> storeDtoList = storeRedisTemplateService.findAll();
+        if(!storeDtoList.isEmpty()){
+            log.info("redis findAll success");
+            return storeDtoList;
+        }
+        else
+            log.info("redis fail");
+
+        //redis 조회 실패
         return storeRepositoryService.findAll().stream()
                 .map(it -> storeConverter.toStoreDto(it))
                 .collect(Collectors.toList());
