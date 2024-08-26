410b506ee0616746b327ba3a3e2c5c0ebe58d183
Gwonjulee <wnfl0925@naver.com>
2024-08-26 17:49:52 +0900

    2차 중간점검_Account: 주석 관리

--- service/account/src/main/java/org/delivery/account/domain/token/service/TokenService.java
+++ service/account/src/main/java/org/delivery/account/domain/token/service/TokenService.java
@@ -10,21 +10,15 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Objects;
 
-/**
- * 토큰에 대한 도메인 로직 담당(Only Token)
- * userId를 받아 토큰 발행
- */
 @Service
 @RequiredArgsConstructor
 public class TokenService {
 
     private final TokenHelperIfs tokenHelperIfs;
 
-    //return userId
     public Long validationToken(String token){
         var map=tokenHelperIfs.validationTokenWithThrow(token);
         var userId=map.get("userId");
-        //validationTokenWithThrow의 파싱을 통해 나온 값에 userId가 없을 경우,
         Objects.requireNonNull(userId,()->{throw new ApiException(ErrorCode.NULL_POINT);
         });
         return Long.parseLong(userId.toString());
