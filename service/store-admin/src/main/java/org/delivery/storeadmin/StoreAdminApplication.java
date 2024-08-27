7bcbfe3522105ace77dfba7dc121e24562781e07
Gwonjulee <wnfl0925@naver.com>
2024-08-28 01:20:47 +0900

    [V3] Eureka 설정: gateway, api, store-admin

--- service/store-admin/src/main/java/org/delivery/storeadmin/StoreAdminApplication.java
+++ service/store-admin/src/main/java/org/delivery/storeadmin/StoreAdminApplication.java
@@ -1,12 +1,13 @@
 package org.delivery.storeadmin;
 
-import org.delivery.db.storemenu.StoreMenuEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class StoreAdminApplication {
     public static void main(String[] args){
         SpringApplication.run(StoreAdminApplication.class,args);
